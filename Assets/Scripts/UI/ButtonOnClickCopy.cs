04da71e85b1d04994c5bc637112368e755fac4df
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2025-11-06 14:17:51 +0000

    Added functionality to copy text to clipboard

---
+++ Assets/Scripts/UI/ButtonOnClickCopy.cs
@@ -0,0 +1,35 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonOnClickCopy : MonoBehaviour
+{
+	[SerializeField]
+	private SystemMessager systemMessager;
+
+	[SerializeField]
+	private string textToCopy;
+
+	[SerializeField]
+	private string feedbackText;
+
+	private Button button;
+
+	void Awake()
+	{
+		button = GetComponent<Button>();
+		button.onClick.AddListener(AddToClipboard);
+	}
+
+	private void AddToClipboard()
+	{
+		GUIUtility.systemCopyBuffer = textToCopy;
+
+		if (!string.IsNullOrEmpty(feedbackText))
+		{
+			systemMessager.Display(feedbackText);
+		}
+	} 
+}
